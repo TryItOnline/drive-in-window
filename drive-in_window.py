cc413382b30af2c47aeb003e774639623849f959
Dennis <dennis@tryitonline.net>
2019-09-16 15:25:00 +0000

    removed trailing whitespace

--- drive-in_window.py
+++ drive-in_window.py
@@ -13,11 +13,11 @@
 # Person 1 would like to know more about this programming language.
 #
 # OK, that will be https://esolangs.org/wiki/Drive-In_Window. Thanks for coming!
- 
+
 import sys
- 
+
 showWarnings = False #Change to true to display warning messages!
- 
+
 def _find_getch():
     try:
         import termios
@@ -25,7 +25,7 @@ def _find_getch():
         # Non-POSIX. Return msvcrt's (Windows') getch.
         import msvcrt
         return msvcrt.getch
- 
+
     # POSIX system. Create and return a getch that manipulates the tty.
     import sys, tty
     def _getch():
@@ -37,44 +37,44 @@ def _find_getch():
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
- 
+
     return _getch
- 
+
 getch = _find_getch()
- 
+
 def printErrorMessage(filename,line_number,line,message):
     errorMessage = "\n"+'Error while parsing \''+filename+'\' at line '+str(line_number) + ' : ' + line
     errorMessage += "\n\t" + message
     print errorMessage
- 
+
 def printWarningMessage(filename,line_number,line,message):
     errorMessage = 'Warning while parsing \''+filename+'\' at line '+str(line_number) + ' : ' + line
     errorMessage += "\n\t" + message
     print errorMessage
- 
+
 if(len(sys.argv) > 1):
     filename = sys.argv[1]
 else:
     print 'File to parse: '
     filename = raw_input().strip()
- 
+
 commands = []
 f = open(filename,'r')
 for line in f:
     commands.append(line.strip())
 f.close()
- 
+
 people = {}
 menu = {}
 for_dict = {}
 end_for_dict = {}
- 
+
 declarationState = 'start'
 errorMessage = ''
 line_number = 0
 success = True
 digits = '0123456789'
- 
+
 while line_number < len(commands):
     line = commands[line_number]
     warningMessage = ''
@@ -95,7 +95,7 @@ while line_number < len(commands):
         if not success:
             errorMessage = 'Invalid Restaurant declaration. First sentence must be in the form of "Hi, welcome to __RESTAURANT_NAME__. Here is a menu."'
         else:
-            declarationState = 'menu'  
+            declarationState = 'menu'
     elif declarationState == 'menu':
         split_line = line.split(':')
         if not len(split_line) == 2:
@@ -156,7 +156,7 @@ while line_number < len(commands):
                     warningMessage = 'Menu entry "'+menu_entry+'" was already declared with a value of $'+str(menu[menu_entry]+'. Old value was replaced.')
                 menu[menu_entry] = int(price)
     elif declarationState == 'main':
-       
+
         #Person N would (not|also) like what Person Y has.
         if ' would ' in line and ' like what Person ' in line:
             split_line = line.split('Person ')
@@ -167,7 +167,7 @@ while line_number < len(commands):
                 person1 = split_line[1].split(' would ')[0].strip()
                 person2 = split_line[2].split(' has.')[0].strip()
                 modifier = split_line[1].split(' would ')[1].split(' like ')[0].strip()
- 
+
                 for digit in person1:
                     if not digit in digits:
                         success = False
@@ -200,8 +200,8 @@ while line_number < len(commands):
                     else:
                         success = False
                         errorMessage = '"' + modifier + '" is not a valid modifier for a "would like" command. Valid modifiers are "would not like" and "would also like".'
-                    people[person1] %= 256                    
- 
+                    people[person1] %= 256
+
         #Person N would (not|also) like menu_item( with side_item|, hold the side_item).
         elif 'Person ' in line and ' would ' in line and ' like ' in line:
             split_line = line.split('Person ')
@@ -273,7 +273,7 @@ while line_number < len(commands):
                         success = False
                         errorMessage = '"' + modifier + '" is not a valid modifier for a "would like" command. Valid modifiers are "would not like" and "would also like".'
                     people[person] %= 256
- 
+
         #Person N will pay for his order!
         elif 'Person ' in line and ' will pay for his order!' in line:
             split_line = line.split('Person ')
@@ -313,7 +313,7 @@ while line_number < len(commands):
                     money = split_line[1].split(' needs ')[1].split(' dollars ')[0].strip()
                 else:
                     money = split_line[1].split(' needs ')[1].split(' dollar ')[0].strip()
-               
+
                 for digit in person:
                     if not digit in digits:
                         success = False
@@ -343,7 +343,7 @@ while line_number < len(commands):
                     elif ' dollars ' in line and money == 1:
                         warningMessage = 'Using "dollars" to reference a single dollar.'
                     people[person] += money
- 
+
         elif 'OK, what should Person ' in line and ' get?' in line:
             person = line.replace('OK, what should Person ','').replace(' get?','').strip()
             for digit in person:
@@ -403,7 +403,7 @@ while line_number < len(commands):
                     errorMessage = 'Person must be greater than 0'
             if success and people[person] == 0:
                 line_number = for_dict[line_number]
- 
+
         elif 'Person ' in line and ' has no more money!' in line:
             if line_number not in end_for_dict:
                 success = False
@@ -412,7 +412,7 @@ while line_number < len(commands):
                 line_number = end_for_dict[line_number]-1
         elif line == 'Just wait while we decide...':
             raw_input()
-       
+
         elif 'OK, that will be ' in line and '. Thanks for coming!' in line:
             declarationState = 'finished'
         else:
@@ -427,6 +427,6 @@ while line_number < len(commands):
         printWarningMessage(filename,line_number,line,warningMessage)
     line_number += 1
 #end while
- 
+
 if declarationState == 'error':
     printErrorMessage(filename,line_number,line,errorMessage)
